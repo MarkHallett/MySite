c9fc78de1137dd17e7a4afc0dcf8916a01d9e59b
Mark Hallett <markhallett@Marks-MBP.connect>
2017-09-13 20:49:49 +0100

    First 30

--- datafeeds/test_eg_feed.py
+++ datafeeds/test_eg_feed.py
@@ -3,48 +3,58 @@
 
 
 
-# read_rates  
-  
-import os  
-  
-def process_row(row):  
-    print row  
-      
-    for k,v in row.items():    
-       print '  ',k,v   
-  
-  
-  
-def run():  
-    print '#################################'  
-      
-    file_dir = r'C:\MH\dev'  
-    file_name = r'CcyRates1Year.csv'  
-      
-    fileName = os.path.join(file_dir,file_name)  
-    print 'fileName', fileName  
-      
-      
-    if not os.path.isfile(fileName):  
-        print 'Error File %s does not exist' %fileName  
-        return  
-      
-    try:  
-        file = open(fileName)  
-        data = file.read()  
-        file.close()  
-    except IOError:  
-        print 'Error reading file %s' %fileName  
-      
-    data = data.split()  
-      
-    headers = data[0].split(',')  
-      
-    for count, row in enumerate(data[1:]):#  
-        print row  
-        r = dict(zip(headers,row.split(',') ))  
-      
-        process_row(r)  
-      
-        break  
-        if count > 3 : break  
+# read_rates
+
+import os
+import time
+
+def process_row(row):
+    print row
+
+    for ccy_pair in ('GBP/USD','USD/EUR','EUR/GBP'):
+         print ccy_pair, row[ccy_pair]
+
+         cmd = 'test_docker_pub_ccy.ksh %s %s' %(ccy_pair, row[ccy_pair])
+         print cmd
+         os.system(cmd)
+         #time.sleep(1)
+
+    #time.sleep(3)
+
+
+
+def run():
+    print '#################################'
+
+    file_dir = r'./data'
+    file_name = r'CcyRates1Year.csv'
+
+    fileName = os.path.join(file_dir,file_name)
+    print 'fileName', fileName
+
+
+    if not os.path.isfile(fileName):
+        print 'Error File %s does not exist' %fileName
+        return
+
+    try:
+        file = open(fileName)
+        data = file.read()
+        file.close()
+    except IOError:
+        print 'Error reading file %s' %fileName
+
+    data = data.split()
+
+    headers = data[0].split(',')
+
+    for count, row in enumerate(data[1:]):#
+        print count
+        r = dict(zip(headers,row.split(',') ))
+
+        process_row(r)
+
+        #break
+        if count > 30 : break
+
+run()
