f7b6bc55ae0f15b32f267edc2458463b5b267eaf
Mark Hallett <markhallett@Marks-MBP.connect>
2017-11-06 22:16:33 +0000

    Missed files

---
+++ sites/main_app/app/local_settings.py
@@ -0,0 +1,33 @@
+import os
+
+# *****************************
+# Environment specific settings
+# *****************************
+
+# DO NOT use "DEBUG = True" in production environments
+DEBUG = True
+
+# DO NOT use Unsecure Secrets in production environments
+# Generate a safe one with:
+#     python -c "import os; print repr(os.urandom(24));"
+SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
+
+# SQLAlchemy settings
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
+
+# Flask-Mail settings
+# For smtp.gmail.com to work, you MUST set "Allow less secure apps" to ON in Google Accounts.
+# Change it in https://myaccount.google.com/security#connectedapps (near the bottom).
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_SSL = False
+MAIL_USE_TLS = True
+#MAIL_USERNAME = 'medaredaltd@gmail.com'
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+#MAIL_PASSWORD = 'XX'
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = '"Your Name AA" <yourname BB @gmail.com>'
+
+ADMINS = [
+    '"Admin One" <admin1@gmail.com>',
+    ]
