3c7b220efafd03f8fd5097f0b8e13cf13e9fbab4
Mark Hallett <markhallett@Marks-MBP.connect>
2018-01-03 07:34:36 +0000

    Volume location for db

--- src/sites/main_app/main_app/app/local_settings.py
+++ src/sites/main_app/main_app/app/local_settings.py
@@ -10,10 +10,13 @@ DEBUG = True
 # DO NOT use Unsecure Secrets in production environments
 # Generate a safe one with:
 #     python -c "import os; print repr(os.urandom(24));"
-SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
+#SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/markhallett/notgoogledrive/github/mysite/app.sqlite'
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 # Flask-Mail settings
 # For smtp.gmail.com to work, you MUST set "Allow less secure apps" to ON in Google Accounts.
@@ -22,9 +25,7 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_SSL = False
 MAIL_USE_TLS = True
-#MAIL_USERNAME = 'medaredaltd@gmail.com'
 MAIL_USERNAME = os.getenv('GMAIL_USERNAME')
-#MAIL_PASSWORD = 'XX'
 MAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = '"Register" <medaredaltd@gmail.com>'
 
